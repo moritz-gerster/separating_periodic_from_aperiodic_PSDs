408e3d4176130062824477cf98675723d4ef08b5
Moritz <moritz.gerster@posteo.de>
2021-06-07 20:13:34 +0200

    Fig0 combine fooof IRASA

--- Fig0_Intro.py
+++ Fig0_Intro.py
@@ -117,8 +117,9 @@ def osc_signals(slope, periodic_params=None, nlv=None, highpass=True,
 
 # Signal params
 srate = 2400
-welch_params_f = dict(fs=srate, nperseg=1*srate)
-welch_params_I = dict(fs=srate, nperseg=4*srate)
+win_sec = 2
+welch_params_f = dict(fs=srate, nperseg=win_sec*srate)
+welch_params_I = dict(fs=srate, nperseg=win_sec*srate)
 
 # Save Path
 fig_path = "../paper_figures/"
@@ -205,55 +206,59 @@ init_ap_fit = gen_aperiodic(fm.freqs,
 init_flat_spec = fm.power_spectrum - init_ap_fit
 
 
-# %% Combine fooof
-
-fig_width = 7.25
-
-fig, axes = plt.subplots(3, 3, figsize=[fig_width*3, 6*3])
-
-# plot initial fit
-ax = axes[0, 0]
-plot_spectrum(fm.freqs, fm.power_spectrum, plt_log,
-              label='Original Power Spectrum', color='black', ax=ax)
-plot_spectrum(fm.freqs, init_ap_fit, plt_log, label='Initial Aperiodic Fit',
-              color='blue', alpha=0.5, linestyle='dashed', ax=ax)
-
-
-# Plot the flattened the power spectrum
-ax = axes[0, 1]
-plot_spectrum(fm.freqs, init_flat_spec, plt_log,
-              label='Flattened Spectrum', color='black', ax=ax)
-
-# show iterations
-ax = axes[0, 2]
-plot_annotated_peak_search_MG(fm, 0, ax)
-
-ax = axes[1, 0]
-plot_annotated_peak_search_MG(fm, 1, ax)
-
-ax = axes[1, 1]
-plot_annotated_peak_search_MG(fm, 2, ax)
-
-
-# plot model
-ax = axes[2, 1]
-plot_spectrum(fm.freqs, fm._peak_fit, plt_log, color='green',
-              label='Final Periodic Fit', ax=ax)
-
-# plot aperiodic
-ax = axes[2, 2]
-plot_spectrum(fm.freqs, fm._spectrum_peak_rm, plt_log,
-              label='Peak Removed Spectrum', color='black', ax=ax)
-plot_spectrum(fm.freqs, fm._ap_fit, plt_log, label='Final Aperiodic Fit',
-              color='blue', alpha=0.5, linestyle='dashed', ax=ax)
-
-# including real data
-# fm.plot(plt_log=plt_log)
+# =============================================================================
+# # %% Combine fooof
+# 
+# fig_width = 7.25
+# 
+# fig, axes = plt.subplots(3, 3, figsize=[fig_width*3, 6*3])
+# 
+# # plot initial fit
+# ax = axes[0, 0]
+# plot_spectrum(fm.freqs, fm.power_spectrum, plt_log,
+#               label='Original Power Spectrum', color='black', ax=ax)
+# plot_spectrum(fm.freqs, init_ap_fit, plt_log, label='Initial Aperiodic Fit',
+#               color='blue', alpha=0.5, linestyle='dashed', ax=ax)
+# 
+# 
+# # Plot the flattened the power spectrum
+# ax = axes[0, 1]
+# plot_spectrum(fm.freqs, init_flat_spec, plt_log,
+#               label='Flattened Spectrum', color='black', ax=ax)
+# 
+# # show iterations
+# ax = axes[0, 2]
+# plot_annotated_peak_search_MG(fm, 0, ax)
+# 
+# ax = axes[1, 0]
+# plot_annotated_peak_search_MG(fm, 1, ax)
+# 
+# ax = axes[1, 1]
+# plot_annotated_peak_search_MG(fm, 2, ax)
+# 
+# # plot aperiodic
+# ax = axes[1, 2]
+# plot_spectrum(fm.freqs, fm._spectrum_peak_rm, plt_log,
+#               label='Peak Removed Spectrum', color='black', ax=ax)
+# plot_spectrum(fm.freqs, fm._ap_fit, plt_log, label='Final Aperiodic Fit',
+#               color='blue', alpha=0.5, linestyle='dashed', ax=ax)
+# 
+# # plot model
+# ax = axes[2, 0]
+# plot_spectrum(fm.freqs, fm._peak_fit, plt_log, color='green',
+#               label='Final Periodic Fit', ax=ax)
+# 
+# # plot model + ap + real
+# ax = axes[2, 1]
+# fm.plot(plt_log=plt_log, ax=ax)
+# =============================================================================
 
 # %% Calc IRASA
-hset = np.arange(1.1, 1.95, 0.05)
+# hset = np.arange(1.1, 1.95, 0.05)
+hset = [1.1, 1.5, 2]
 
-irasa_params = dict(sf=srate, band=freq_range, win_sec=4, hset=hset)
+irasa_params = dict(sf=srate, band=freq_range,
+                    win_sec=win_sec, hset=hset)
 
 IRASA = irasa(data=toy_comb, **irasa_params)
 
@@ -261,13 +266,19 @@ freqs_irasa, psd_ap, psd_osc, params = IRASA
 
 psd_ap, psd_osc = psd_ap[0], psd_osc[0]
 
-plt.loglog(freqs_irasa, psd_ap)
-plt.loglog(freqs_irasa, psd_osc)
+
+IR_offset = params["Intercept"][0]
+IR_slope = -params["Slope"][0]
+
+psd_fit = gen_aperiodic(freqs_irasa, (IR_offset, IR_slope))
+
+# plt.loglog(freqs_irasa, psd_ap)
+# plt.loglog(freqs_irasa, psd_osc)
 
 # %% 1
 
-hset = [1.1, 1.15, 1.2, 1.25, 1.3, 1.35, 1.4, 1.45, 1.5, 1.55, 1.6,
-          1.65, 1.7, 1.75, 1.8, 1.85, 1.9]
+# hset = [1.1, 1.15, 1.2, 1.25, 1.3, 1.35, 1.4, 1.45, 1.5, 1.55, 1.6,
+#          1.65, 1.7, 1.75, 1.8, 1.85, 1.9]
 hset = [1.1, 1.5, 2]
 
 win = welch_params_I["nperseg"]
@@ -286,75 +297,98 @@ for i, h in enumerate(hset):
     
     psds_resampled[i, :] = np.sqrt(psd_up * psd_dw)
 
-    
-for i, h in enumerate(hset):
-    plt.loglog(psds_resampled[i])
-
 # Now we take the median PSD of all the resampling factors, which gives
 # a good estimate of the aperiodic component of the PSD.
-psd_aperiodic = np.median(psds_resampled, axis=0)
+psd_median = np.median(psds_resampled, axis=0)
 
-plt.loglog(psd_aperiodic)
+# Mask
+mask = (freqs_I <= 100)
+freqs_I = freqs_I[mask]
+psd_median = psd_median[mask]
+psd_comb_I = psd_comb_I[mask]
+psds_resampled = psds_resampled[:, mask]
 
 # We can now calculate the oscillations (= periodic) component.
-psd_osc = psd_comb_I - psd_aperiodic
+# psd_osc = psd_comb_I - psd_aperiodic
+# =============================================================================
+# # %% Combine IRASA
+# 
+# fig, axes = plt.subplots(3, 2,  figsize=[fig_width*3, 6*3], sharex=False)
+# 
+# ax = axes[0, 0]
+# ax.loglog(freqs_I, psd_comb_I, "k", label="Original Data")
+# ax.legend()
+# 
+# ax = axes[0, 1]
+# ax.loglog(freqs_I, psd_comb_I, "k", label="Original Data")
+# for i in range(len(hset)):
+#     ax.loglog(freqs_I, psds_resampled[i], label=f"h={hset[i]}")
+# ax.legend()
+# 
+# ax = axes[1, 0]
+# ax.loglog(freqs_I, psd_median, label="Median of resampled PSDs")
+# ax.legend()
+# 
+# ax = axes[1, 1]
+# ax.semilogx(freqs_irasa, psd_osc, label="Oscillatory Component")
+# ax.legend()
+# 
+# ax = axes[2, 0]
+# ax.loglog(freqs_I, psd_comb_I, "k", label="Original Data")
+# ax.loglog(freqs_irasa, psd_ap, label="Aperiodic")
+# ax.loglog(freqs_irasa, psd_ap + psd_osc, label="Model")
+# ax.loglog(freqs_irasa, 10**psd_fit, label="Fit")
+# ax.legend()
+# =============================================================================
 
-IR_offset = params["Intercept"][0]
-IR_slope = -params["Slope"][0]
+# %% Compare fooof IRASA
+    
+# to do:
+    # combine fooof and IRASA where they are similar
+    # split resampling in 3 itereations and visualize taking the median
 
-psd_fit = gen_aperiodic(freqs_irasa, (IR_offset, IR_slope))
+# panel 1: original data for fooof and IRASA with two different nperseg
+# panel 2: 5 steps fooof, 3 steps IRASA
+# panel 3: combine aperiodic psd + fit of fooof and IRASA in one figure
+# (consider plotting the ground truth)
+# panel 4: combine oscillatory components fooof and IRASA in one figure
+# (consider plotting the ground truth)
+# panel 5: show full model fooof and IRASA and original
 
-# %% Combine IRASA
-mask = freqs_I < 100
+# decide: window 4 sec IRASA and 1 sec fooof or both 2 sec?
 
-fig, axes = plt.subplots(2, 2,  figsize=[fig_width*3, 6*3], sharex=False)
+# add R^2 and exponents
 
-ax = axes[0, 0]
-ax.loglog(freqs_I[mask], psd_comb_I[mask])
-
-ax = axes[0, 1]
-ax.loglog(freqs_I[mask], psd_comb_I[mask], "k")
-for i in range(len(hset)):
-    ax.loglog(freqs_I[mask], psds_resampled[i, mask])
-
-ax = axes[1, 0]
-ax.loglog(freqs_I[mask], psd_aperiodic[mask])
-
-ax = axes[1, 1]
-ax.semilogx(freqs_I[mask], psd_osc[mask])
-
-    # %% Compare fooof IRASA
-
+fig_width = 7.25
 
 fig = plt.figure(figsize=[fig_width*3, 5.5*3], constrained_layout=True)
 
-gs0 = gridspec.GridSpec(3, 1, figure=fig, height_ratios=[1, 1, 1])
+gs0 = gridspec.GridSpec(2, 1, figure=fig, height_ratios=[3, 2])
 
-gs00 = gs0[0].subgridspec(1, 5, width_ratios=[5, 5, 2, 2, 2])
+gs00 = gs0[0].subgridspec(2, 5)
 
-ax1 = fig.add_subplot(gs00[0])
-ax2 = fig.add_subplot(gs00[1])
-ax3 = fig.add_subplot(gs00[2])
-ax4 = fig.add_subplot(gs00[3])
-ax5 = fig.add_subplot(gs00[4])
+ax1 = fig.add_subplot(gs00[:, 0])
+ax2 = fig.add_subplot(gs00[0, 1])
+ax3 = fig.add_subplot(gs00[0, 2])
+ax4 = fig.add_subplot(gs00[1, 1])
+ax5 = fig.add_subplot(gs00[1, 2])
+ax6 = fig.add_subplot(gs00[0, 3])
+ax7 = fig.add_subplot(gs00[1, 3])
+ax8 = fig.add_subplot(gs00[:, 4])
 
-gs01 = gridspec.GridSpecFromSubplotSpec(1, 3, subplot_spec=gs0[1])
-ax6 = fig.add_subplot(gs01[0])
-ax7 = fig.add_subplot(gs01[1])
-ax8 = fig.add_subplot(gs01[2])
-
-gs02 = gridspec.GridSpecFromSubplotSpec(1, 3, subplot_spec=gs0[2])
-ax9 = fig.add_subplot(gs02[0])
-#ax7 = fig.add_subplot(gs01[1])
-#ax8 = fig.add_subplot(gs01[2])
-
-
+gs01 = gridspec.GridSpecFromSubplotSpec(2, 4, subplot_spec=gs0[1])
+ax9 = fig.add_subplot(gs01[:, 0])
+ax10 = fig.add_subplot(gs01[:, 1])
+ax11 = fig.add_subplot(gs01[0, 2])
+ax12 = fig.add_subplot(gs01[1, 2])
+ax13 = fig.add_subplot(gs01[:, 3])
 
 ax = ax1
 plot_spectrum(fm.freqs, fm.power_spectrum, plt_log,
               label='Original Power Spectrum', color='black', ax=ax)
 plot_spectrum(fm.freqs, init_ap_fit, plt_log, label='Initial Aperiodic Fit',
               color='blue', alpha=0.5, linestyle='dashed', ax=ax)
+
 
 
 # Plot the flattened the power spectrum
@@ -381,40 +415,210 @@ ax.set_yticks([])
 ax.set_yticklabels([])
 
 
-
-# IRASA
 ax = ax6
-ax.loglog(freqs_I, psd_comb_I)
+plot_spectrum(fm.freqs, fm._spectrum_peak_rm, plt_log,
+              label='Peak Removed Spectrum', color='black', ax=ax)
+plot_spectrum(fm.freqs, fm._ap_fit, plt_log, label='Final Aperiodic Fit',
+              color='blue', alpha=0.5, linestyle='dashed', ax=ax)
+
 
 ax = ax7
-ax.loglog(freqs_I, psd_comb_I, "k")
-for i in range(len(hset)):
-    ax.loglog(freqs_I, psds_resampled[i])
-    
+plot_spectrum(fm.freqs, fm._peak_fit, plt_log, color='green',
+              label='Final Periodic Fit', ax=ax)
+
 ax = ax8
-ax.loglog(freqs_I, psd_aperiodic, label="IRASA")
-ax.loglog(freqs_irasa, 10**psd_fit, label="IRASA fit")
-ax.loglog(fm.freqs, 10**fm._spectrum_peak_rm, label="fooof")
-ax.loglog(fm.freqs, 10**fm._ap_fit, label="fooof fit")
+fm.plot(plt_log=plt_log, plot_aperiodic=False, ax=ax)
+
+
+# IRASA
+ax = ax9
+ax.loglog(freqs_I, psd_comb_I, "k", label="Original Data")
 ax.legend()
 
-ax = ax9
-ax.plot(fm.freqs, 10**fm._peak_fit, label="osc fooof")
-osc_irasa *= 1e12
-osc_irasa = np.log10(osc_irasa)
-osc_irasa = np.nan_to_num(osc_irasa)
-ax.plot(freqs_I, osc_irasa, label="osc IRASA")
-ax.set_xlim(1, 100)
+ax = ax10
+ax.loglog(freqs_I, psd_comb_I, "k", label="Original Data")
+for i in range(len(hset)):
+    ax.loglog(freqs_I, psds_resampled[i], label=f"h={hset[i]}")
+ax.legend()
+
+ax = ax11
+ax.loglog(freqs_I, psd_median, label="Median of resampled PSDs")
+ax.loglog(freqs_irasa, 10**psd_fit, label="Fit")
+ax.legend()
+
+ax = ax12
+ax.semilogx(freqs_irasa, psd_osc, label="Oscillatory Component")
+ax.legend()
+
+ax = ax13
+ax.loglog(freqs_I, psd_comb_I, "k", label="Original Data")
+# ax.loglog(freqs_irasa, psd_ap, label="Aperiodic")
+ax.loglog(freqs_irasa, psd_ap + psd_osc, label="Aperiodic + Osc")
+
 ax.legend()
 
 plt.show()
+
+# %
+
+# =============================================================================
+# fig, ax = plt.subplots(1, 1)
+# ax.loglog(freqs_irasa, psd_ap, label="IRASA")
+# ax.loglog(freqs_irasa, 10**psd_fit, label="IRASA fit")
+# ax.loglog(fm.freqs, 10**fm._spectrum_peak_rm, label="fooof")
+# ax.loglog(fm.freqs, 10**fm._ap_fit, label="fooof fit")
+# ax.legend()
+# 
+# 
+# fig, ax = plt.subplots(1, 1)
+# ax.plot(fm.freqs, 10**fm._peak_fit, label="osc fooof")
+# osc_irasa = psd_osc * 1e12
+# osc_irasa = np.log10(osc_irasa)
+# osc_irasa = np.nan_to_num(osc_irasa)
+# ax.plot(freqs_irasa, osc_irasa, label="osc IRASA")
+# ax.plot(freqs_f, psd_osc_f * 1e9, label="ground osc")
+# ax.plot(freqs_f, np.log10(psd_osc_f*1e12), label="ground osc")
+# ax.set_xlim(1, 100)
+# ax.legend()
+# =============================================================================
+
 # %%
+fig = plt.figure(figsize=[fig_width*3, 5.5*3], constrained_layout=True)
+
+gs0 = gridspec.GridSpec(2, 1, figure=fig, height_ratios=[3, 2])
+
+gs00 = gs0[0].subgridspec(4, 6)
+
+ax1 = fig.add_subplot(gs00[:, 0])
+ax = ax1
+ax.loglog(freqs_I, psd_comb_I, "k", label="Original Data")
+ax.legend()
+
+
+ax2 = fig.add_subplot(gs00[0, 1])
+ax = ax2
+plot_spectrum(fm.freqs, fm.power_spectrum, plt_log,
+              label='Original Power Spectrum', color='black', ax=ax)
+plot_spectrum(fm.freqs, init_ap_fit, plt_log, label='Initial Aperiodic Fit',
+              color='blue', alpha=0.5, linestyle='dashed', ax=ax)
+
+
+ax3 = fig.add_subplot(gs00[0, 2])
+ax = ax3
+plot_spectrum(fm.freqs, init_flat_spec, plt_log,
+              label='Flattened Spectrum', color='black', ax=ax)
+
+
+ax4 = fig.add_subplot(gs00[0, 3])
+ax = ax4
+plot_annotated_peak_search_MG(fm, 0, ax)
+ax.get_legend().remove()
+
+
+ax5 = fig.add_subplot(gs00[0, 4])
+ax = ax5
+plot_annotated_peak_search_MG(fm, 1, ax)
+ax.get_legend().remove()
+ax.set_ylabel("")
+ax.set_yticks([])
+ax.set_yticklabels([])
+
+
+ax6 = fig.add_subplot(gs00[1, 4])
+ax = ax6
+plot_annotated_peak_search_MG(fm, 2, ax)
+ax.set_ylabel("")
+ax.set_yticks([])
+ax.set_yticklabels([])
+
+
+ax7 = fig.add_subplot(gs00[1, 3])
+ax = ax7
+plot_spectrum(fm.freqs, fm._peak_fit, plt_log, color='green',
+              label='Final Periodic Fit', ax=ax)
+
+
+ax8 = fig.add_subplot(gs00[1, 2])
+ax = ax8
+plot_spectrum(fm.freqs, fm._spectrum_peak_rm, plt_log,
+              label='Peak Removed Spectrum', color='black', ax=ax)
+plot_spectrum(fm.freqs, fm._ap_fit, plt_log, label='Final Aperiodic Fit',
+              color='blue', alpha=0.5, linestyle='dashed', ax=ax)
+
+
+ax9 = fig.add_subplot(gs00[1, 1])
+ax = ax9
+fm.plot(plt_log=plt_log, plot_aperiodic=False, ax=ax)
 
 
 
 
 
 
-# Plot full model, created by combining the peak and aperiodic fits
-plot_spectrum(fm.freqs, fm.fooofed_spectrum_, plt_log,
-              label='Full Model', color='red')
+
+
+
+ax10 = fig.add_subplot(gs00[3, 1])
+ax = ax10
+ax.loglog(freqs_I, psd_comb_I, "k", label="Original Data")
+i = 0
+ax.loglog(freqs_I, psds_resampled[i], label=f"h={hset[i]}")
+ax.legend()
+
+ax11 = fig.add_subplot(gs00[3, 2])
+ax = ax11
+ax.loglog(freqs_I, psd_comb_I, "k", label="Original Data")
+i = 1
+ax.loglog(freqs_I, psds_resampled[i], label=f"h={hset[i]}")
+ax.legend()
+
+ax12 = fig.add_subplot(gs00[3, 3])
+ax = ax12
+ax.loglog(freqs_I, psd_comb_I, "k", label="Original Data")
+i = 2
+ax.loglog(freqs_I, psds_resampled[i], label=f"h={hset[i]}")
+ax.legend()
+
+ax13 = fig.add_subplot(gs00[2, 3])
+ax = ax13
+ax.loglog(freqs_I, psd_median, label="Median of resampled PSDs")
+ax.loglog(freqs_irasa, 10**psd_fit, label="Fit")
+ax.legend()
+
+ax14 = fig.add_subplot(gs00[2, 2])
+ax = ax14
+ax.semilogx(freqs_irasa, psd_osc, label="Oscillatory Component")
+ax.legend()
+
+ax15 = fig.add_subplot(gs00[2, 1])
+ax = ax15
+ax.loglog(freqs_I, psd_comb_I, "k", label="Original Data")
+# ax.loglog(freqs_irasa, psd_ap, label="Aperiodic")
+ax.loglog(freqs_irasa, psd_ap + psd_osc, label="Aperiodic + Osc")
+
+ax.legend()
+
+plt.show()
+
+# %
+
+# =============================================================================
+# fig, ax = plt.subplots(1, 1)
+# ax.loglog(freqs_irasa, psd_ap, label="IRASA")
+# ax.loglog(freqs_irasa, 10**psd_fit, label="IRASA fit")
+# ax.loglog(fm.freqs, 10**fm._spectrum_peak_rm, label="fooof")
+# ax.loglog(fm.freqs, 10**fm._ap_fit, label="fooof fit")
+# ax.legend()
+# 
+# 
+# fig, ax = plt.subplots(1, 1)
+# ax.plot(fm.freqs, 10**fm._peak_fit, label="osc fooof")
+# osc_irasa = psd_osc * 1e12
+# osc_irasa = np.log10(osc_irasa)
+# osc_irasa = np.nan_to_num(osc_irasa)
+# ax.plot(freqs_irasa, osc_irasa, label="osc IRASA")
+# ax.plot(freqs_f, psd_osc_f * 1e9, label="ground osc")
+# ax.plot(freqs_f, np.log10(psd_osc_f*1e12), label="ground osc")
+# ax.set_xlim(1, 100)
+# ax.legend()
+# =============================================================================
