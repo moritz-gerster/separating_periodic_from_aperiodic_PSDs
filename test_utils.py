ed234f0e72364ef31352e44faa41e1184e3d7ad3
Moritz <moritz.gerster@posteo.de>
2021-10-15 15:41:14 +0200

    Update test_utils.py

--- test_utils.py
+++ test_utils.py
@@ -1,15 +1,16 @@
+import numpy as np
 from utils import elec_phys_signal
 
 
 # Test simulation of electrophysiological signals
 def test_elec_phys_signal():
-    
+
     # test output
     output = elec_phys_signal(1)
     assert isinstance(output, tuple)
     assert isinstance(output[0], np.ndarray)
     assert isinstance(output[1], np.ndarray)
-    
+
     # test impact of 1/f exponent
     assert not np.allclose(elec_phys_signal(1)[0],
                            elec_phys_signal(2)[0])
@@ -29,7 +30,7 @@ def test_elec_phys_signal():
     # test impact of highpass
     assert not np.allclose(elec_phys_signal(1, highpass=1)[0],
                            elec_phys_signal(1, highpass=0)[0])
-    
+
     # test duration of signal
     assert elec_phys_signal(1, sample_rate=24, duration=1)[0].shape[0] == 24-2
     assert elec_phys_signal(1, sample_rate=1, duration=24)[0].shape[0] == 24-2
